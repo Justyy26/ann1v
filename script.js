8592b825c1060d73894db272c5aad7fab0d56ea1
Justyy26 <jazmacatuno@gmail.com>
2026-02-06 19:55:34 +0800

    Update script.js
    mixed extension fix

--- script.js
+++ script.js
@@ -2,22 +2,36 @@ function loadMonth(monthName, shortName, totalPhotos) {
   const container = document.getElementById(monthName);
 
   for (let i = 1; i <= totalPhotos; i++) {
+
     const img = document.createElement("img");
-    img.src = `images/${monthName}/${shortName}${i}.jpg`;
     img.loading = "lazy";
+
+    // Try JPG first
+    img.src = `images/${monthName}/${shortName}${i}.jpg`;
+
+    // If JPG fails → try JPEG
+    img.onerror = () => {
+      img.onerror = () => {
+        // If JPEG fails → try PNG
+        img.src = `images/${monthName}/${shortName}${i}.png`;
+      };
+
+      img.src = `images/${monthName}/${shortName}${i}.jpeg`;
+    };
+
     container.appendChild(img);
   }
 }
 
 /* CHANGE PHOTO COUNTS HERE */
-loadMonth("february", "feb", 10);
-loadMonth("march", "mar", 10);
-loadMonth("april", "apr", 10);
-loadMonth("may", "may", 10);
-loadMonth("june", "jun", 10);
-loadMonth("july", "jul", 10);
-loadMonth("august", "aug", 10);
-loadMonth("september", "sep", 10);
-loadMonth("october", "oct", 10);
-loadMonth("november", "nov", 10);
-loadMonth("december", "dec", 10);
+loadMonth("february", "feb", 12);
+loadMonth("march", "mar", 20);
+loadMonth("april", "apr", 20);
+loadMonth("may", "may", 20);
+loadMonth("june", "jun", 20);
+loadMonth("july", "jul", 20);
+loadMonth("august", "aug", 20);
+loadMonth("september", "sep", 20);
+loadMonth("october", "oct", 20);
+loadMonth("november", "nov", 20);
+loadMonth("december", "dec", 20);
